b8ed63af37b2d346ba871c3d89f735d098778cbf
nguyenduchoang2907 <135149449+nguyenduchoang2907@users.noreply.github.com>
2023-10-05 20:58:23 +0700

    New init

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,76 @@
 import React from "react";
+import { ADD_TO_CART, UPDATE_CART, DELETE_CART } from "../../shared/constants/action-type";
+import { useSelector, useDispatch } from "react-redux";
+import { getImageProduct } from "../../shared/ultils";
+
 const Cart = () => {
+    const dispatch = useDispatch();
+
+    const carts= useSelector(({Cart})=>{
+        console.log(Cart.items);
+        return Cart.items;
+        
+    });
+    
+
+
+  
+    const onChangeQty=(e,id)=>{
+        const value=parseInt(e.target.value);
+        if(value<=0){
+            // eslint-disable-next-line no-restricted-globals
+            let isConfirm=confirm("Bạn có chắc chắn muốn xoá không?");
+            if(isConfirm){
+                dispatch(
+                    {
+                        type: DELETE_CART,
+                        payload: {
+                            _id:id,
+                            
+                        },
+                    }
+                )
+            }
+            else{
+                dispatch({
+                    type: UPDATE_CART,
+                    payload: {
+                        _id:id,
+                        qty: 1,
+                    },
+                })
+            }
+        }else{
+            dispatch({
+                type:UPDATE_CART,
+                payload:{
+                    _id:id,
+                    qty:value,
+                }
+            });
+        }
+       
+    }
+    const onDeleteItem = (e, id) => {
+        
+         // eslint-disable-next-line no-restricted-globals
+         let isConfirm=confirm("Bạn có chắc chắn muốn xoá không?");
+         if(isConfirm){
+             dispatch(
+                 {
+                     type: DELETE_CART,
+                     payload: {
+                         _id:id,
+                         
+                     },
+                 }
+             )
+         }
+         else{
+          return   false
+         }
+     }
+    
     return (
         <>
             <div>
@@ -11,62 +82,34 @@ const Cart = () => {
                         <div className="cart-nav-item col-lg-3 col-md-3 col-sm-12">Giá</div>
                     </div>
                     <form method="post">
-                        <div className="cart-item row">
-                            <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
-                                <img src="images/product-1.png" />
-                                <h4>iPhone Xs Max 2 Sim - 256GB Gold</h4>
-                            </div>
-                            <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
-                                <input type="number" id="quantity" className="form-control form-blue quantity" defaultValue={1} min={1} />
-                            </div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>32.990.000đ</b><a href="#">Xóa</a></div>
-                        </div>
-                        <div className="cart-item row">
-                            <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
-                                <img src="images/product-2.png" />
-                                <h4>iPhone Xs Max 2 Sim - 256GB Gold</h4>
-                            </div>
-                            <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
-                                <input type="number" id="quantity" className="form-control form-blue quantity" defaultValue={1} min={1} />
-                            </div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>32.990.000đ</b><a href="#">Xóa</a></div>
-                        </div>
-                        <div className="cart-item row">
-                            <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
-                                <img src="images/product-3.png" />
-                                <h4>iPhone Xs Max 2 Sim - 256GB Gold</h4>
-                            </div>
-                            <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
-                                <input type="number" id="quantity" className="form-control form-blue quantity" defaultValue={1} min={1} />
-                            </div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>32.990.000đ</b><a href="#">Xóa</a></div>
-                        </div>
-                        <div className="cart-item row">
-                            <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
-                                <img src="images/product-4.png" />
-                                <h4>iPhone Xs Max 2 Sim - 256GB Gold</h4>
-                            </div>
-                            <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
-                                <input type="number" id="quantity" className="form-control form-blue quantity" defaultValue={1} min={1} />
-                            </div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>32.990.000đ</b><a href="#">Xóa</a></div>
-                        </div>
-                        <div className="cart-item row">
-                            <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
-                                <img src="images/product-5.png" />
-                                <h4>iPhone Xs Max 2 Sim - 256GB Gold</h4>
-                            </div>
-                            <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
-                                <input type="number" id="quantity" className="form-control form-blue quantity" defaultValue={1} min={1} />
-                            </div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>32.990.000đ</b><a href="#">Xóa</a></div>
-                        </div>
-                        <div className="row">
+                        {
+                            carts.map((item)=>
+                                    <div className="cart-item row">
+                                    <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
+                                        <img src={getImageProduct(item.image)} />
+                                        <h4>{item.name}</h4>
+                                    </div>
+                                    <div className="cart-quantity col-lg-2 col-md-2 col-sm-12">
+                                        <input
+                                        onChange={(e) => onChangeQty(e, item._id)}
+                                            type="number"
+                                            id="quantity"
+                                            className="form-control form-blue quantity"
+                                            value={item.qty} />
+                                    </div>
+                                    <div onClick={(e) => onDeleteItem(e, item._id)} className="cart-price col-lg-3 col-md-3 col-sm-12"><b>
+                            {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.qty * item.price)}</b><a href="#">Xóa</a></div>
+                                </div>
+                            )
+                        }
+                           <div className="row">
                             <div className="cart-thumb col-lg-7 col-md-7 col-sm-12">
                                 <button id="update-cart" className="btn btn-success" type="submit" name="sbm">Cập nhật giỏ hàng</button>
                             </div>
                             <div className="cart-total col-lg-2 col-md-2 col-sm-12"><b>Tổng cộng:</b></div>
-                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>88.970.000đ</b></div>
+                            <div className="cart-price col-lg-3 col-md-3 col-sm-12"><b>
+                            {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(carts?.reduce((total, item) => total + item.qty * item.price, 0))}
+                                </b></div>
                         </div>
                     </form>
                 </div>
--- src/redux-setup/reducers/cart.js
+++ src/redux-setup/reducers/cart.js
@@ -1,25 +1,53 @@
-import { ADD_TO_CART } from "../../shared/constants/action-type";
+import { ADD_TO_CART, DELETE_CART, UPDATE_CART } from "../../shared/constants/action-type";
 
-const initState={
-    items:[],
+const initState = {
+    items: [],
 }
-export default (state=initState,action)=>{
-switch(action.type){
-    case  ADD_TO_CART: return addToCart(state,action.payload);//payload chua thong tin gio hang vua cap nhat
-    default: return state;
+export default (state = initState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART: return addToCart(state, action.payload);//payload chua thong tin gio hang vua cap nhat
+        case UPDATE_CART:return updateCart(state,action.payload);
+        case DELETE_CART: 
+        const newItems=state.items.filter((item)=>item._id!=action.payload._id);
+        return {...state,items:newItems};
+        default: return state;
+
+    }
 }
-}
-const addToCart=(state,payload)=>{
+const addToCart = (state, payload) => {
     const items = state.items;
     let isProductExists;
-    items.map((item)=>{
-        if(item._id=payload._id){
-            item.qty+=payload.qty;
-            isProductExists=true;
-        }return item;
+    items.map((item) => {
+        if (item._id === payload._id) {
+            item.qty += payload.qty;
+            isProductExists = true;
+        } return item;
     })
-const newItems=isProductExists? items:[...items,payload];//bien newitems chua mang moi neu isProductExists la true thi giu nguyen gia tri cua items neu la false thi tao mang moi 
-//bang cach them payload vao cuoi mang items
-localStorage.setItem("cart_items",JSON.stringify(newItems));
-return{...state,items:newItems}//tra ve 1 state
+    const newItems = isProductExists ? items : [...items, payload];//bien newitems chua mang moi neu isProductExists la true thi giu nguyen gia tri cua items neu la false thi tao mang moi 
+    //bang cach them payload vao cuoi mang items
+    localStorage.setItem("cart_items", JSON.stringify(newItems));
+    return { ...state, items: newItems }//tra ve 1 state
 }
+const updateCart = (state, payload)=>{
+    const items = state.items;
+    const {_id, qty} = payload;
+
+    const newCarts = items.map((item)=>{
+        if(item._id === _id){
+            item.qty = qty;
+        }
+        return item;
+    });
+    return {...state, items: newCarts}
+}
+// const deleteCart = (state, payload)=>{
+//     const items = state.items;
+//     const newCarts = items.filter((item)=>{
+//         if(item._id === payload.id){
+//             return false;
+//         } 
+//         return true;
+//     });
+
+//     return {...state, items: newCarts}
+// }
